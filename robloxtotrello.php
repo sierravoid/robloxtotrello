7836ebb10277a6c1139829a98afbe752d9d74bd3
Sierra <sierra.void@gmail.com>
2017-07-02 22:36:08 -0700

    asd

--- robloxtotrello.php
+++ robloxtotrello.php
@@ -50,7 +50,8 @@ foreach ($Cards as $Card) {
 			echo "Current Rank: {$UserRank}<br><br>";
 			
 			$rankurl = "http://obscure-harbor-96531.herokuapp.com/setRank/2518831/{$UserID}/{$Rank}";
-			$rankdata = json_encode(array("key" => "{$BotKey}"));
+			$rankdata = "{\"key\":\"{$BotKey}\"}";
+			echo $rankdata;
 			
 			$rankoptions = array(
 				'http' => array(
