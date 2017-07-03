5dd16d2b872374cfaaf21523b2c8e7b98d147a16
Sierra <sierra.void@gmail.com>
2017-07-02 18:40:32 -0700

    s

--- robloxtotrello.php
+++ robloxtotrello.php
@@ -4,11 +4,11 @@
 
 <?php
 
-$data = json_decode($HTTP_RAW_POST_DATA, true);
+//$data = json_decode($HTTP_RAW_POST_DATA, true);
 
-$Key = $data["Key"];
-$Token = $data["Token"];
-$BotKey = $data["BotKey"];
+$Key = $_GET["Key"];
+$Token = $_GET["Token"];
+$BotKey = $_GET["BotKey"];
 
 $cardscurl = curl_init("https://api.trello.com/1/lists/595987df3e10c1bea15389d0/cards?key={$Key}&token={$Token}");
 curl_setopt($cardscurl, CURLOPT_RETURNTRANSFER, true);
