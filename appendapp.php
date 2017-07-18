1112ff707b5f31766135dc81c92baa51c7184b48
Sierra <sierra.void@gmail.com>
2017-07-17 18:47:31 -0700

    YE

--- appendapp.php
+++ appendapp.php
@@ -1,15 +1,15 @@
 <?php
 
-$Key = $_GET["key"];
-$Token = $_GET["token"];
+//$Key = $_GET["key"];
+//$Token = $_GET["token"];
 $CardID = $_GET["cardid"];
 $Passed = $_GET["passed"];
 $PlayerName = $_GET["playername"];
 
-function Comment($TheComment) {
+function Comment($TheComment, $ID) {
 	$commentdata = json_encode(array("text" => $TheComment, "key" => $_GET["key"], "token" => $_GET["token"]));
 	
-	$commentcurl = curl_init("https://api.trello.com/1/cards/{$CardID}/actions/comments");
+	$commentcurl = curl_init("https://api.trello.com/1/cards/{$ID}/actions/comments");
 	curl_setopt($commentcurl, CURLOPT_CUSTOMREQUEST, "POST");
 	curl_setopt($commentcurl, CURLOPT_POSTFIELDS, $commentdata);
 	curl_setopt($commentcurl, CURLOPT_RETURNTRANSFER, true);
@@ -19,13 +19,10 @@ function Comment($TheComment) {
 	);
 	
 	$commentresult = curl_exec($commentcurl);
-	
-	echo "{$CardID}<br>";
-	echo $commentresult;
 }
 
 if ($Passed == "true") {
-	Comment("{$PlayerName} passed this application.");
+	Comment("{$PlayerName} passed this application.", $CardID);
 	
 	$movedata = json_encode(array("value" => "595987df3e10c1bea15389d0", "key" => $_GET["key"], "token" => $_GET["token"]));
 			
@@ -41,7 +38,7 @@ if ($Passed == "true") {
 	$moveresult = curl_exec($movecurl);
 	
 } elseif ($Passed == "false") {
-	Comment("{$PlayerName} rejected this application.");
+	Comment("{$PlayerName} rejected this application.", $CardID);
 	
 	$movedata = json_encode(array("value" => "5956aa5375f28113f8489b47", "key" => $_GET["key"], "token" => $_GET["token"]));
 			
